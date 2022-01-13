6d571ac5930c34040d7a093bbbcb14b20e06665f
Jaime Moscoso Bernal <jaimesfc@live.com>
2022-01-14 00:48:20 +0100

    Simulacro completado

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,31 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+import org.springframework.samples.petclinic.model.NamedEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    Integer id;
-    String name;
-    double price;
-    ProductType productType;
+@Entity
+@Table(name="products")
+public class Product extends NamedEntity{
+
+	@Min(0)
+	@Column(name="price")
+	Double price;
+	
+	@ManyToOne
+	@JoinColumn(name="product_type_id")
+	ProductType productType;
 }
